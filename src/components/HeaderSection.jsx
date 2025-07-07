c8b63b2eeac9d70ad2f8ac3adfcb720684a7095d
Azamat-Kasylov <follower.az@gmail.com>
2025-07-07 11:50:31 +0700

    added components

---
+++ src/components/HeaderSection.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable jsx-a11y/alt-text */
+const HeaderSection = () => {
+  return (
+    <header className="header">
+      <img src="https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjEwNDktMjIucG5n.png" />
+    </header>
+  );
+};
+
+export default HeaderSection;
